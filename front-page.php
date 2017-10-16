c8626af3172d0f1a2786d26107153ce889427192
Bradley Kin Fong <kin@ministrycentered.com>
2017-10-16 16:29:12 -0500

    add blue header filler to project

--- front-page.php
+++ front-page.php
@@ -11,24 +11,22 @@
 	      	<figure class="slider">
 	      		<figure>
 	            <div class="figure-overlay"></div>
-	            <img src="https://az616578.vo.msecnd.net/files/2016/10/21/636126250118128281844548477_Worship-Background_crop1.jpg" alt>
+							<img src="<?php echo get_template_directory_uri(); ?>/img/lakesideheader.png" alt="Find">
 	            <figcaption>find.</figcaption>
 	      		</figure>
 	      		<figure>
 	            <div class="figure-overlay"></div>
-	            <img src="https://az616578.vo.msecnd.net/files/2016/10/21/636126250118128281844548477_Worship-Background_crop1.jpg" alt>
-	      			<!-- <img src="http://www.brasch-group.com/images/Aug-09-1.jpg" alt> -->
+							<img src="<?php echo get_template_directory_uri(); ?>/img/lakesideheader.png" alt="Follow">
 	      			<figcaption>follow.</figcaption>
 	      		</figure>
 	      		<figure>
 	            <div class="figure-overlay"></div>
-	            <img src="https://az616578.vo.msecnd.net/files/2016/10/21/636126250118128281844548477_Worship-Background_crop1.jpg" alt>
-	      			<!-- <img src="https://www.loveworldsat.org/wp-content/uploads/2017/05/wcs_zim-1.jpg" alt> -->
+							<img src="<?php echo get_template_directory_uri(); ?>/img/lakesideheader.png" alt="Flavor">
 	      			<figcaption>flavor.</figcaption>
 	      		</figure>
 	      		<figure>
 	            <div class="figure-overlay"></div>
-	      			<img src="https://az616578.vo.msecnd.net/files/2016/10/21/636126250118128281844548477_Worship-Background_crop1.jpg" alt>
+							<img src="<?php echo get_template_directory_uri(); ?>/img/lakesideheader.png" alt="Find">
 	      			<figcaption>find.</figcaption>
 	      		</figure>
 	      	</figure>
@@ -47,7 +45,7 @@
 					</p>
 				</div>
 				<div class="map">
-					<div id="map" style="width:auto;height:400px;"></div>
+					<div id="map" style="width:100%;height:400px;"></div>
 					<script>
 						function initMap() {
 							var lakeside = {lat: 42.018694, lng: -87.727839};
@@ -66,44 +64,8 @@
 				</div>
 			</div>
 
-
-
-			<!-- <h1><?php the_title(); ?></h1> -->
-
-		<!-- <?php if (have_posts()): while (have_posts()) : the_post(); ?> -->
-
-			<!-- article -->
-			<!-- <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> -->
-
-				<!-- <?php the_content(); ?> -->
-
-				<!-- <?php comments_template( '', true ); // Remove if you don't want comments ?> -->
-
-				<!-- <br class="clear"> -->
-
-				<!-- <?php edit_post_link(); ?> -->
-
-			<!-- </article> -->
-			<!-- /article -->
-
-		<!-- <?php endwhile; ?> -->
-
-		<!-- <?php else: ?> -->
-
-			<!-- article -->
-			<!-- <article> -->
-
-				<!-- <h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2> -->
-
-			<!-- </article> -->
-			<!-- /article -->
-
-		<!-- <?php endif; ?> -->
-
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
